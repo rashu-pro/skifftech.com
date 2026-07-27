e5786e29ad9b05fc2eb98a7bc98338bd4fa175d7
Nayeem <snayeem978@gmail.com>
2026-07-27 14:44:05 +0600

    Eye brow removed

--- wp-content/themes/skifftech/template-parts/contact/hero.php
+++ wp-content/themes/skifftech/template-parts/contact/hero.php
@@ -2,9 +2,9 @@
   <div class="ct-wrap">
     <div class="reveal">
       <nav class="ct-crumb" aria-label="Breadcrumb">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a><span class="sep">/</span><span class="cur">Contact Us</span>
+        <a href="<?php echo esc_url(home_url('/')); ?>">Home</a><span class="sep">/</span><span class="cur">Contact Us</span>
       </nav>
-      <span class="eyebrow">Contact us</span>
+
       <h1>Let&rsquo;s start a <span class="gold-txt">conversation.</span></h1>
       <p class="ct-lead">You are more than welcome to leave your contact info and we&rsquo;ll be in touch shortly.</p>
     </div>
