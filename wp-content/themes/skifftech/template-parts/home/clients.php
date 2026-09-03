14e6b7f54290031c8d8346dd3176334f8a3d164e
rashu-pro <rashu.pro@gmail.com>
2026-09-03 20:44:12 +0600

    client logo style changed

--- wp-content/themes/skifftech/template-parts/home/clients.php
+++ wp-content/themes/skifftech/template-parts/home/clients.php
@@ -8,32 +8,36 @@
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/Logo_TWK_2018-NoTag.webp' ); ?>" alt="TWK">
       </a>
       <a class="client-logo" href="#">
-        <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/DominionDMS_Logo.min.jpg' ); ?>" alt="Masjid Solutions">
+        <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/DominionDMS_Logo.min.jpg' ); ?>" alt="Dominion DMS">
       </a>
-      <a class="client-logo" href="#">
+      <a class="client-logo has-label" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/lgcl-itt.png' ); ?>" alt="LGCL ITT">
+        <span class="client-name">LGCL ITT</span>
       </a>
       <a class="client-logo" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/ausxdesign.png' ); ?>" alt="ausxdesign">
       </a>
-      <a class="client-logo" href="#">
+      <a class="client-logo has-label" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/logo-hhc.png' ); ?>" alt="HHC">
+        <span class="client-name">Heritage Home Construction</span>
       </a>
       <!-- duplicate set for seamless loop -->
        <a class="client-logo" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/Logo_TWK_2018-NoTag.webp' ); ?>" alt="TWK">
       </a>
       <a class="client-logo" href="#">
-        <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/DominionDMS_Logo.min.jpg' ); ?>" alt="Masjid Solutions">
+        <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/DominionDMS_Logo.min.jpg' ); ?>" alt="Dominion DMS">
       </a>
-      <a class="client-logo" href="#">
+      <a class="client-logo has-label" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/lgcl-itt.png' ); ?>" alt="LGCL ITT">
+        <span class="client-name">LGCL ITT</span>
       </a>
       <a class="client-logo" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/ausxdesign.png' ); ?>" alt="ausxdesign">
       </a>
-      <a class="client-logo" href="#">
+      <a class="client-logo has-label" href="#">
         <img src="<?php echo esc_url( get_template_directory_uri() . '/images/clients/logo-hhc.png' ); ?>" alt="HHC">
+        <span class="client-name">Heritage Home Construction</span>
       </a>
     </div>
   </div>
